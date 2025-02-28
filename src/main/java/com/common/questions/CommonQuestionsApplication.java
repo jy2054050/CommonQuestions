e52f17b8f17d4701e9ae86d3d1ebebdf4baf3272
Jitender.Yadav <jy2054050@gmail.com>
2025-02-28 21:11:49 +0530

    Boilerplate code

---
+++ src/main/java/com/common/questions/CommonQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.common.questions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonQuestionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/common/questions/CommonQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.common.questions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
